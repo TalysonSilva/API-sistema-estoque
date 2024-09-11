a661e2c294948c0ac7f903589f4e4ca59848fa7c
TalysonSilva <talyson.rodigues@gmail.com>
2024-09-10 22:46:53 -0300

    refactor: ProdutoService

--- src/main/java/com/app/sistema_de_controle_de_estoque/service/ProdutoService.java
+++ src/main/java/com/app/sistema_de_controle_de_estoque/service/ProdutoService.java
@@ -17,29 +17,23 @@ public class ProdutoService {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    private Produto produto = new Produto();
-
     public List<Produto> todosProdutos() {
         return  produtoRepository.findAll();
     }
 
     public String cadastroNovoProduto(ProdutoDTO novoProduto) {
 
-
-        if (novoProduto == null || novoProduto.getNome() == null
-                || novoProduto.getPreco() <= 0 || novoProduto.getQuantidade() <= 0) {
+        if (!isProdutoValido(novoProduto)) {
            return "Dados do produto inválidos";
         }
 
         if (produtoRepository.existsByNome(novoProduto.getNome())){
             return "Produto já existe";
         }
-        produto.setNome(novoProduto.getNome());
-        produto.setPreco(novoProduto.getPreco());
-        produto.setQuantidade(novoProduto.getQuantidade());
 
-
+        Produto produto = mapProduto(novoProduto);
         produtoRepository.save(produto);
+
         return "Novo Produto cadastrado com Sucesso";
 
     }
@@ -48,7 +42,7 @@ public class ProdutoService {
     public String removerProdutos(Long id) {
 
         if (!produtoRepository.existsById(id)) {
-           throw new EntityNotFoundException("Produto não exite");
+           throw new EntityNotFoundException("Produto não existe");
         }
 
         produtoRepository.deleteById(id);
@@ -62,24 +56,59 @@ public class ProdutoService {
 
     public String atualizarProduto(Long id, ProdutoDTO atualizado) {
 
-        return produtoRepository.findById(id).map(produto -> {
+        //Verificar se o produto existe
 
-            produto.setNome(atualizado.getNome());
-            produto.setPreco(atualizado.getPreco());
-            produto.setQuantidade(atualizado.getQuantidade());
-            produtoRepository.save(produto);
+        Optional<Produto> produtoExisteOpt = produtoRepository.findById(id);
+        if (produtoExisteOpt.isPresent()) {
 
+            Produto produtoExite = produtoExisteOpt.get();
+
+            //converte o ProdutoDTO para produto
+            if (atualizado.getQuantidade() > 0){
+                produtoExite.setQuantidade(atualizado.getQuantidade());
+            }
+
+            if (atualizado.getPreco() != null) {
+                produtoExite.setPreco(atualizado.getPreco());
+            }
+            if (atualizado.getNome() != null){
+                produtoExite.setNome(atualizado.getNome());
+            }
+            if (atualizado.getDescricao() != null){
+                produtoExite.setDescricao(atualizado.getDescricao());
+            }
+
+            if (atualizado.getCategoria() != null){
+                produtoExite.setCategoria(atualizado.getCategoria());
+            }
+
+            produtoRepository.save(produtoExite);
             return "Produto atualizado com sucesso";
 
-        }).orElseGet(() -> {
+        }
 
-            produto.setQuantidade(atualizado.getQuantidade());
-            produto.setNome(atualizado.getNome());
-            produto.setPreco(atualizado.getPreco());
+        return "Produto não encontrado";
+    }
 
-            produtoRepository.save(produto);
-            return "Produto não existia, foi adicionado à base de dados";
 
-        });
+    //Converte ProdutoDTO para  Produto
+    private Produto mapProduto(ProdutoDTO produtoDTO) {
+        Produto produto = new Produto();
+        produto.setNome(produtoDTO.getNome());
+        produto.setDescricao(produtoDTO.getDescricao());
+        produto.setQuantidade(produtoDTO.getQuantidade());
+        produto.setPreco(produtoDTO.getPreco());
+        produto.setCategoria(produtoDTO.getCategoria());
+        return produto;
+    }
+
+    //Verificar se o produto é valido
+    private boolean isProdutoValido(ProdutoDTO produtoDTO){
+        return produtoDTO != null
+                && produtoDTO.getNome() != null && !produtoDTO.getNome().isBlank()
+                && produtoDTO.getPreco() > 0
+                && produtoDTO.getQuantidade() > 0
+                && produtoDTO.getCategoria() != null && !produtoDTO.getCategoria().isBlank()
+                && produtoDTO.getDescricao() != null && !produtoDTO.getDescricao().isBlank();
     }
 }
